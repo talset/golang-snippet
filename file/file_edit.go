5268a0274ec08b9dc0ae76d48e27e8529697468b
talset <florian.lambert@enovance.com>
2016-06-22 11:47:18 +0200

    file/file_edit.go with gofmt

--- file/file_edit.go
+++ file/file_edit.go
@@ -2,51 +2,51 @@ package main
 
 import (
 	"fmt"
-  "os"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
-func writefile(path string, content string, number int){
+func writefile(path string, content string, number int) {
 	fmt.Printf("writefile %s\n", path)
 	// create file
 	f, err := os.Create(path)
 	if err != nil {
 		panic(err)
-  }
+	}
 
-  // close fi on exit and check for its returned error
+	// close fi on exit and check for its returned error
 	defer func() {
-			if err := f.Close(); err != nil {
-					panic(err)
-			}
-  }()
-	for i := 0;  i<number; i++ {
-		    // write x lines with content
-				f.Write([]byte(fmt.Sprintf("%d %s\n", i, content)))
-  }
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	for i := 0; i < number; i++ {
+		// write x lines with content
+		f.Write([]byte(fmt.Sprintf("%d %s\n", i, content)))
+	}
 
 }
 
 func readfile(path string) []byte {
 	fmt.Printf("readfile %s\n", path)
-  // Open
+	// Open
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-			panic(err)
+		panic(err)
 	}
 	// fmt.Println(string(data))
 	return data
 }
 
-func changefile(file []byte, pattern string, change string){
-  fmt.Println(string(file))
-  // ioutil.WriteFile("filename", contents, 0644)
+func changefile(file []byte, pattern string, change string) {
+	fmt.Println(string(file))
+	// ioutil.WriteFile("filename", contents, 0644)
 	lines := strings.Split(string(file), "\n")
 	for i, line := range lines {
-					if strings.Contains(line, pattern) {
-				      lines[i] = strings.Replace(line, pattern, change, -1)
-					}
+		if strings.Contains(line, pattern) {
+			lines[i] = strings.Replace(line, pattern, change, -1)
+		}
 	}
 	changedfile := strings.Join(lines, "\n")
 	fmt.Println(changedfile)
@@ -54,7 +54,7 @@ func changefile(file []byte, pattern string, change string){
 
 func main() {
 
-  writefile("/tmp/test", "bla bla\ntest test", 10)
+	writefile("/tmp/test", "bla bla\ntest test", 10)
 	infile := readfile("/tmp/test")
 	changefile(infile, "bla", "foobar")
 }
